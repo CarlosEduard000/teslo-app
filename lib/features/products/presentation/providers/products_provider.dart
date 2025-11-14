2ac7f7dab95ba301cd4c919c58c59736e7eb7b67
CarlosEduard000 <eduardparedesgonza@gmail.com>
2025-11-13 19:57:58 -0500

    second commit

--- lib/features/products/presentation/providers/products_provider.dart
+++ lib/features/products/presentation/providers/products_provider.dart
@@ -21,6 +21,30 @@ class ProductsNotifier extends StateNotifier<ProductsState>{
     loadNextPage();
   }
 
+  Future<bool> createOrUpdateProduct( Map<String, dynamic> productLike) async{
+    try {
+      final product = await productsRepository.createUpdateProduct(productLike);
+      final isProductInList = state.products.any((element) => element.id == product.id);
+
+      if( !isProductInList){
+        state =state.copyMith(
+          products: [...state.products, product]
+        );
+        return true;
+      }
+
+      state = state.copyMith(
+        products: state.products.map(
+          (element) => ( element.id == product.id ) ? product : element,
+        ).toList()
+      );
+      return true;
+
+    } catch (e) {
+      return false;
+    }
+  }
+
   Future loadNextPage() async {
 
     if(state.isLoading || state.isLastPage) return;
